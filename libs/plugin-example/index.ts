75837f6df3bd71504c584c7cd7d6abf85e9221ed
ebalo <ebalo55@users.noreply.github.com>
2023-06-24 10:19:09 +0200

    docs: minor additions

--- libs/plugin-example/index.ts
+++ libs/plugin-example/index.ts
@@ -1,4 +1,8 @@
+// Import the module from the src folder and export it as default entrypoint
 import { PluginExampleModule } from "./src";
 
-export * from "./src";
 export default PluginExampleModule;
+
+// Optionally (but suggested) also re-export all the modules and utilities from the src folder in order to enhance
+// the developer experience
+export * from "./src";
