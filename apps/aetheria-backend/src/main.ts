cf7da4771df40f77b9334cc02ddf031200bfd670
ebalo <ebalo55@users.noreply.github.com>
2023-07-01 18:27:43 +0200

    feat: docker base image building steps

--- apps/aetheria-backend/src/main.ts
+++ apps/aetheria-backend/src/main.ts
@@ -4,7 +4,7 @@ import { bootstrap } from "@aetheria/common";
 
 /**
  * Bootstrap the application.
- * The bootstraping logic have been extracted to a separate file to make it easier to test and maintain.
+ * The bootstrapping logic have been extracted to a separate file to make it easier to test and maintain.
  */
 bootstrap({
 	enable_native_logging: true,
