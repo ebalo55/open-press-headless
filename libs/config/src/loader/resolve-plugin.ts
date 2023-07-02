66f08ad2d9865a290c68a5ef2ef94c793988589b
ebalo <ebalo55@users.noreply.github.com>
2023-07-02 16:29:28 +0200

    feat: plugin resolution added the possibility to define the resolution path

--- libs/config/src/loader/resolve-plugin.ts
+++ libs/config/src/loader/resolve-plugin.ts
@@ -6,22 +6,32 @@ import { cwd } from "node:process";
 /**
  * Try to resolve the package.json file from the given plugin from the given resolution path or the parent directory
  * @param {AetheriaPlugin} plugin The plugin to resolve
+ * @param resolution_path The path to the plugins resolution directory, if not given, the current working directory is
+ *     used
  * @returns {Promise<string>} The package.json file content
  */
-const tryPackageJsonResolutionPaths = async (plugin: AetheriaPlugin): Promise<string> => {
+const tryPackageJsonResolutionPaths = async (plugin: AetheriaPlugin, resolution_path?: string): Promise<string> => {
+	resolution_path = resolution_path ?? cwd();
+
 	try {
-		return await readFile(resolve(cwd(), plugin.resolve, "package.json"), "utf-8");
-	} catch (e: any) {
-		return await readFile(resolve(cwd(), plugin.resolve, "..", "package.json"), "utf-8");
+		return await readFile(resolve(resolution_path, plugin.resolve, "package.json"), "utf-8");
+	}
+	catch (e: any) {
+		return await readFile(resolve(resolution_path, plugin.resolve, "..", "package.json"), "utf-8");
 	}
 };
 
 /**
  * Resolve the given plugin
  * @param plugin The plugin to resolve
+ * @param resolution_path The path to the plugins resolution directory, if not given, the current working directory is
+ *     used
  */
-export const resolvePlugin = async (plugin: AetheriaPlugin): Promise<AetheriaResolvedPlugin> => {
-	const package_json = JSON.parse(await tryPackageJsonResolutionPaths(plugin));
+export const resolvePlugin = async (
+	plugin: AetheriaPlugin,
+	resolution_path?: string,
+): Promise<AetheriaResolvedPlugin> => {
+	const package_json = JSON.parse(await tryPackageJsonResolutionPaths(plugin, resolution_path));
 
 	// load assets from package.json if defined
 	let assets: string[] | undefined = undefined;
--- libs/config/src/loader/resolve-plugins.ts
+++ libs/config/src/loader/resolve-plugins.ts
@@ -3,12 +3,17 @@ import { resolvePlugin } from "./resolve-plugin";
 
 /**
  * Resolve the plugins from the configuration
- * @param configuration
+ * @param configuration The configuration to resolve the plugins from
+ * @param resolution_path The path to the plugins resolution directory, if not given, the current working directory is
+ *    used
  */
-export const resolvePlugins = async (configuration: AetheriaConfiguration): Promise<AetheriaResolvedPlugin[]> => {
+export const resolvePlugins = async (
+	configuration: AetheriaConfiguration,
+	resolution_path?: string,
+): Promise<AetheriaResolvedPlugin[]> => {
 	return await Promise.all(
 		configuration.plugins.map(async (plugin): Promise<AetheriaResolvedPlugin> => {
-			return await resolvePlugin(plugin);
-		})
+			return await resolvePlugin(plugin, resolution_path);
+		}),
 	);
 };
