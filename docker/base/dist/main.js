accf4a64450527f79b100a5a95f2cc50769214e5
ebalo <ebalo55@users.noreply.github.com>
2023-07-02 15:59:54 +0200

    perf: gitignore update

--- docker/base/dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@aetheria/common");
-(0, common_1.bootstrap)({
-	enable_native_logging: true,
-	enable_error_logging: true,
-});
-//# sourceMappingURL=main.js.map
