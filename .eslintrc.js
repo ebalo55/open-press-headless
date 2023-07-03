43454d82f673c29813fad0b0454bbb60ab3f7841
ebalo <ebalo55@users.noreply.github.com>
2023-07-03 16:46:27 +0200

    refactor: dead cli removal

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,11 +14,12 @@ module.exports = {
 	},
 	ignorePatterns: [".eslintrc.js"],
 	rules: {
-		"@typescript-eslint/interface-name-prefix": "off",
+		"@typescript-eslint/interface-name-prefix":         "off",
 		"@typescript-eslint/explicit-function-return-type": "off",
 		//'@typescript-eslint/explicit-module-boundary-types': 'off',
-		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-explicit-any":       "off",
 		"@typescript-eslint/mixed-spaces-and-tabs": "off",
-		"prettier/prettier": "off",
+		"prettier/prettier":                        "off",
+		"node/no-unresolved-import":                "on",
 	},
 };
